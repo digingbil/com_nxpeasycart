b291da0614c4727f0357eac2df4c8b47566f8b7e
Zoran Tanevski <zoran.tanevski@gmail.com>
2025-12-17 18:53:45 +0100

    Fix deprecation

--- administrator/components/com_nxpeasycart/src/Service/Import/ImportExportService.php
+++ administrator/components/com_nxpeasycart/src/Service/Import/ImportExportService.php
@@ -12,8 +12,7 @@ namespace Joomla\Component\Nxpeasycart\Administrator\Service\Import;
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
-use Joomla\CMS\Filesystem\File;
-use Joomla\CMS\Filesystem\Folder;
+use Joomla\Filesystem\Folder;
 use Joomla\Database\DatabaseInterface;
 use Joomla\Component\Nxpeasycart\Administrator\Service\SettingsService;
 
